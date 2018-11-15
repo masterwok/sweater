90475cfbeb25b573509640339687804f1d65694f
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2018-11-15 03:53:11 -0500

    master - Fixed namespace..added collection.

--- Sweater.Core/Services/IndexerQueryService.cs
+++ Sweater.Core/Services/IndexerQueryService.cs
@@ -1,11 +1,15 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Sweater.Core.Indexers.Contracts;
 using Sweater.Core.Models;
+using Sweater.Core.Services.Contracts;
 
-namespace Sweater.Core.Services.Contracts
+namespace Sweater.Core.Services
 {
     public class IndexerQueryService : IIndexerQueryService
     {
+        private IList<IIndexer> _indexers = new List<IIndexer>();
+
         public Task LoadIndexers()
         {
             throw new System.NotImplementedException();
