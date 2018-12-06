197f36c07f3ac5eb4878aa5cddc951a442d1d7e1
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2018-12-05 22:26:45 -0500

    master - TorrentExtensions Tests and Clearing Cache

---
+++ Sweater.Api.Test/Extensions/TorrentExtensions.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using System.Linq;
+using AutoFixture;
+using NUnit.Framework;
+using Sweater.Api.Extensions;
+using Sweater.Core.Constants;
+using Sweater.Core.Models;
+
+namespace Sweater.Api.Test.Extensions
+{
+    [TestFixture]
+    public class TorrentExtensions
+    {
+        private static readonly Fixture _fixture = new Fixture();
+
+
+        private static readonly List<Torrent> Torrents = new List<Torrent>
+        {
+            _fixture.Create<Torrent>(),
+            _fixture.Create<Torrent>(),
+            _fixture.Create<Torrent>(),
+            _fixture.Create<Torrent>()
+        };
+
+        private static readonly List<IndexerResult> IndexerResults = new List<IndexerResult>
+        {
+            new IndexerResult {Indexer = Indexer.Rarbg.ToString(), Torrents = Torrents},
+            new IndexerResult {Indexer = Indexer.LeetX.ToString(), Torrents = Torrents},
+            new IndexerResult {Indexer = Indexer.ThePirateBay.ToString(), Torrents = Torrents}
+        };
+
+        [Test]
+        public void FlattenIndexerResults_Should_Have_Entries_For_Each_Indexer()
+        {
+            var flattenedResults = IndexerResults.FlattenIndexerResults();
+
+            var groups = flattenedResults
+                .GroupBy(g => new {Indexer = g.Indexer.ToString()})
+                .ToList();
+
+            Assert.AreEqual(IndexerResults.Count, groups.Count);
+        }
+
+        [Test]
+        public void FlattenIndexerResults_Should_Have_Torrents_For_Each_Indexer()
+        {
+            var flattenedResults = IndexerResults.FlattenIndexerResults();
+
+            var groups = flattenedResults
+                .GroupBy(g => new {Indexer = g.Indexer.ToString()})
+                .ToList();
+
+            var torrents = groups
+                .SelectMany(t => t)
+                .ToList();
+
+            foreach (var indexerResult in IndexerResults)
+            {
+                Assert.AreEqual(
+                    Torrents.Count
+                    , torrents.Count(t => t.Indexer == indexerResult.Indexer.ToString())
+                );
+            }
+        }
+    }
+}
