63a0188188bff3cc32a1fb0627a133fdef849ec7
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2019-01-24 20:16:27 -0500

    sorting - Added SortBy and OrderBy Props to Query

--- Sweater.Core/Models/Query.cs
+++ Sweater.Core/Models/Query.cs
@@ -1,7 +1,5 @@
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using Sweater.Core.Attributes;
 using Sweater.Core.Constants;
 
 namespace Sweater.Core.Models
@@ -14,13 +12,32 @@ namespace Sweater.Core.Models
     [SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Global")]
     public class Query
     {
+        /// <summary>
+        /// What indexers to use for the query.
+        /// </summary>
         public Indexer[] Indexers { get; set; } = new Indexer[0];
 
+        /// <summary>
+        /// The query string (i.e. what the user is trying to find)
+        /// </summary>
         public string QueryString { get; set; }
+
+        /// <summary>
+        /// What torrent attribute to sort by.
+        /// </summary>
+        public SortField SortBy { get; set; }
+
+        /// <summary>
+        /// What direction to order sorted attribute by.
+        /// </summary>
+        public SortOrder OrderBy { get; set; }
 
         public override string ToString() => $"Indexer Tag = {IndexersString}, " +
                                              $"Query String = {QueryString}";
 
+        /// <summary>
+        /// Create a comma-delimited string of indexers specified in this query.
+        /// </summary>
         private string IndexersString => string.Join(
             ", "
             , Indexers.Select(i => i.ToString())
