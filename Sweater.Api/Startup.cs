f18f079662a8a4898d1badf9f0e383e9969ee37c
Jonathan Trowbridge <jonathan.trowbridge@protonmail.com>
2019-11-17 22:34:34 -0500

    dotnet3 - Initial Progress

--- Sweater.Api/Startup.cs
+++ Sweater.Api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Sweater.Api.Filters;
 using Sweater.Api.Services;
 using Sweater.Core.Attributes;
@@ -47,13 +48,14 @@ namespace Sweater.Api
                 // CORS must be enabled before MVC
                 .AddCors()
                 .AddMemoryCache()
-                .AddMvc(options =>
-                {
-                    // Controller filter attribute
-                    options.Filters.Add<CatchAllExceptionFilter>();
-                    options.Filters.Add<ValidModelStateFilter>();
-                })
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+                .AddControllers();
+//                .AddMvc(options =>
+//                {
+//                    // Controller filter attribute
+//                    options.Filters.Add<CatchAllExceptionFilter>();
+//                    options.Filters.Add<ValidModelStateFilter>();
+//                })
+
 
             // Filters
             services.AddScoped<CatchAllExceptionFilter>();
@@ -116,7 +118,7 @@ namespace Sweater.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -129,7 +131,9 @@ namespace Sweater.Api
 
             app.UseHttpsRedirection();
             app.UseCors(options => options.AllowAnyOrigin());
-            app.UseMvc();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
 }
